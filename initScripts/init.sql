7f9373d4986ef399409b49ca8f79915d45b26b10
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2023-09-28 23:56:09 +0100

    insert some dummy data to the sql table

--- initScripts/init.sql
+++ initScripts/init.sql
@@ -3,11 +3,11 @@ GO
 USE `khalid`;
 GO
 CREATE TABLE `tests` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   `results` varchar(30) NOT NULL,
-  `no_tests` int(11) NOT NULL
+  `no_tests` int(11) NOT NULL,
+   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 GO
-ALTER TABLE `tests`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `tests` (name, results, no_tests) VALUES ('test1', 'positive', '1'), ('test2', 'positive', '3'), ('test3', 'negative', '5'), ('test4', 'neutral', '10');
